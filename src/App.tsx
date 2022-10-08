47f81153bf16d77a69676fca04f40f6051ad181b
Kunal Sangtiani <kunalsangtiani03@gmail.com>
2022-10-08 16:13:18 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -27,10 +27,11 @@ export function App() {
     transactionsByEmployeeUtils.invalidateData()
 
     await employeeUtils.fetchAll()
+    
+    setIsLoading(false)
     await paginatedTransactionsUtils.fetchAll()
 
 
-    setIsLoading(false)
   }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
 
   const loadTransactionsByEmployee = useCallback(
